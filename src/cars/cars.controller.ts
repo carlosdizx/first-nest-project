d272d68eb13ea806506cebc28a614f46a992540f
Carlos Díaz <ernesto.diaz@finaktiva.com>
2022-11-16 20:05:58 -0500

    :sparkles: feat: updated controller, methods post, patch and delete add in the controller

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -12,5 +21,23 @@ export class CarsController {
   @Get(':id')
   findOneById(@Param('id', ParseIntPipe) id: number) {
     return this.service.findOneById(+id);
+  }
+
+  @Post()
+  createCar(@Body() body: any) {
+    return body;
+  }
+
+  @Patch(':id')
+  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'delete',
+      id,
+    };
   }
 }
