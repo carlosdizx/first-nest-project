89b8964398ad333f080511a7ab979de52cf2e6a3
Carlos Díaz <ernesto.diaz@finaktiva.com>
2022-11-25 10:38:15 -0500

    :sparkles: add validation and properties the dto

--- src/cars/create-car.dto.ts
+++ src/cars/create-car.dto.ts
@@ -1,9 +1,10 @@
-import { IsString } from 'class-validator';
+import { IsString, MinLength } from "class-validator";
 
 export class CreateCarDto {
   @IsString()
   private readonly brand: string;
 
   @IsString()
+  @MinLength(3)
   private readonly model: string;
 }
