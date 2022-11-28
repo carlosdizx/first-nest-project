27e1ccfd8454fb3fe25af940b2159a136edba37c
Carlos Díaz <ernesto.diaz@finaktiva.com>
2022-11-27 23:53:52 -0500

    :sparkles: updated npm and service from brand

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,19 +1,34 @@
-import { Injectable } from '@nestjs/common';
-import { CreateBrandDto } from './dto/create-brand.dto';
-import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateBrandDto } from "./dto/create-brand.dto";
+import { UpdateBrandDto } from "./dto/update-brand.dto";
+import { Brand } from "./entities/brand.entity";
+import { v4 as uuid } from "uuid";
 
 @Injectable()
 export class BrandsService {
+
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: "Toyoya",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ];
+
   create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+    return "This action adds a new brand";
   }
 
   findAll() {
-    return `This action returns all brands`;
+    return this.brands;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  findOne(id: string) {
+    const brand: Brand = this.brands.find(brand => brand.id === id);
+    if (brand)
+      return brand;
+    throw new NotFoundException("The brand not found");
   }
 
   update(id: number, updateBrandDto: UpdateBrandDto) {
