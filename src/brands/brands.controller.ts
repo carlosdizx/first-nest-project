9ce0b5ac714b9fb4cc246b026ad2899d4c2d6229
Carlos Díaz <ernesto.diaz@finaktiva.com>
2022-11-28 00:16:26 -0500

    :bug: updated brand.controller.ts

--- src/brands/brands.controller.ts
+++ src/brands/brands.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { BrandsService } from './brands.service';
-import { CreateBrandDto } from './dto/create-brand.dto';
-import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from "@nestjs/common";
+import { BrandsService } from "./brands.service";
+import { CreateBrandDto } from "./dto/create-brand.dto";
+import { UpdateBrandDto } from "./dto/update-brand.dto";
 
-@Controller('brands')
+@Controller("brands")
 export class BrandsController {
-  constructor(private readonly brandsService: BrandsService) {}
+  constructor(private readonly brandsService: BrandsService) {
+  }
 
   @Post()
   create(@Body() createBrandDto: CreateBrandDto) {
@@ -17,18 +18,18 @@ export class BrandsController {
     return this.brandsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id", ParseUUIDPipe) id: string) {
     return this.brandsService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBrandDto: UpdateBrandDto) {
+  @Patch(":id")
+  update(@Param("id", ParseUUIDPipe) id: string, @Body() updateBrandDto: UpdateBrandDto) {
     return this.brandsService.update(id, updateBrandDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id", ParseUUIDPipe) id: string) {
     return this.brandsService.remove(id);
   }
 }
